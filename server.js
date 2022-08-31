74c20f4e8b0142e0cf72b7e81b556ee52a3e604f
My nikname "ton <68073926+ThechoiceMaster@users.noreply.github.com>
2022-08-31 18:00:10 +0700

    auth

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/line/redirect', async (req, res) => {
     const bodyToken = new URLSearchParams({
       grant_type: 'authorization_code',
       code: req.query.code,
-      redirect_uri: `${process.env.LINE_ENDPOIN}/auth/line/redirect`,
+      redirect_uri: `${process.env.LINE_ENDPOIN}/line/redirect`,
       client_id: process.env.LINE_CLIENT_ID,
       client_secret: process.env.LINE_CLIENT_SECRET,
     })
